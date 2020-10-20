17519bed6c507d94864af68c63574e62f804628a
Lauren Crow <hi@lcrow.studio>
2020-10-20 13:09:52 +0200

    Set up basic files, write pseudocode/comments

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//get input from user and assign to variable - player's choice:
+
+//compute result of 'round'
+  //generate computer's choice and assign it to variable
+  //compare computer's choice with player's choice
+//previous step x 5
+//output result of round
+//compute overall game stats
+
+//output result of game
