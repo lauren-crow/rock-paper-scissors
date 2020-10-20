844e32abda93ee49fa00d4f524cbe62609233b82
Lauren Crow <hi@lcrow.studio>
2020-10-20 20:06:36 +0200

    clean up playRound with message constant, implement score counter, cleanup / format

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+let playerScore = 0;    //initializes scores
+let computerScore = 0;
+let tieCount = 0;
+
 function playRound() {
-    function playerPlay() {             //get input from user and assign to variable - player's choice:
+    function playerPlay() {             //get input from user and assign to variable - return
         let playerSelection = prompt(/*"Round " + round + */": Rock paper scissors, 1-2-3! Type your choice:");
         console.log('You chose '+ playerSelection);
         return playerSelection;
         }
 
-    function computerPlay() {           //generate computer's choice and assign it to variable
+    function computerPlay() {           //generate computer's choice and assign it to variable - return
         let choiceArray = ["rock","paper","scissors"]                               //array of choices
         let randomChoice = Math.floor(Math.random()*choiceArray.length);            //random choice of index
         let computerSelection = choiceArray[randomChoice];                          // choose string associated with chosen index
@@ -13,48 +17,49 @@ function playRound() {
         return computerSelection;
         }
 
-    const playerSelection = playerPlay();
-    const computerSelection = computerPlay();
+    const playerSelection = playerPlay();        //set playerSelection const
+    const computerSelection = computerPlay();    //set computerSelction const
                             
     function compare(playerSelection, computerSelection) {    //compare computer's choice with playe r's choice and print results  
             
         //set result variables    
-        let tie = "- tie";      
-        let win = " - you win round";
-        let loss = "- computer wins round";
+        let tie = "    Tied round";      
+        let win = "   You win the round!";
+        let loss = "   Computer wins the round.";
 
         if (playerSelection == computerSelection) {
+            tieCount = tieCount +1; // add point to tie count
             return tie;
         }
         else {
             if (playerSelection == 'rock') {
                 if (computerSelection == 'scissors') {
-                    return win;
-                    let playerScore = playerScore + 1;  // add point to playerScore   
+                    playerScore = playerScore + 1;  // add point to playerScore  
+                    return win; 
                 }
                 else if (computerSelection == 'paper') {
+                    computerScore = computerScore + 1; // add point to computerPoint
                     return loss;
-                    let computerScore = computerScore +1; // add point to computerPoint
                 }
             }
             else if (playerSelection == 'paper') {
                 if (computerSelection == 'rock'){
-                    return win;
-                    let playerScore = playerScore + 1;  // add point to playerScore  
+                    playerScore = playerScore + 1;  // add point to playerScore 
+                    return win;  
                 }
                 else if (computerSelection == 'scissors'){
+                    computerScore = computerScore + 1;  // add point to computerPoint
                     return loss;
-                    let computerScore = computerScore +1; // add point to computerPoint
                 }
             }
             else if (playerSelection == 'scissors') {
                 if (computerSelection == 'paper'){
-                    return win;
-                    let playerScore = playerScore + 1;  // add point to playerScore 
+                    playerScore = playerScore + 1;   // add point to playerScore
+                    return win;  
                 }
                 else if (computerSelection == 'rock'){
+                    computerScore = computerScore + 1;  // add point to computerPoint
                     return loss;
-                    let computerScore = computerScore +1; // add point to computerPoint
                 }
                 
             }
@@ -63,33 +68,39 @@ function playRound() {
 
     function getResult() {
         let result = compare(playerSelection, computerSelection);
-        console.log("result is " + result);
+        console.log(result);
+    }
+
+    function getScore() {        
+        console.log("Your score: " + playerScore + " Computer score: " + computerScore + " Tie count: " + tieCount);
     }
 
     getResult();
+    getScore();
+}
+
+function getScore() {
+
 }
 
 
-function game() {     //keep score   //report winner at the end
+function game() {          //loops round five times                                          //to-do:   //report winner at the end
     for (let i = 0; i <= 4; i++) { //loop round * 5 for whole game
 
         let round = i + 1;
 
         playRound();
 
-        /*if (round == 0) {
-            let playerScore = 0;   //initialize playerScore
-            let computerScore = 0;       //initialize computerScore
-        }
-        else if (result == win) {
-            playerScore+=1
-        }
-        else if (result == lose) {
-            computerScore+=1
-        }
-            
-        console.log(result + playerScore + computerScore)*/
+    }
 
+    if (playerScore == computerScore) {
+        console.log("It's a draw");
+    }
+    else if (playerScore > computerScore) {
+        console.log("You win!!");
+    }
+    if (playerScore < computerScore) {
+        console.log("You lose, loser! The computer beat you.");
     }
 }
 
