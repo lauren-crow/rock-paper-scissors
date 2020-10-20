235b3f53afdaba7e6ad10fb5573472b0735c1bc7
Lauren Crow <hi@lcrow.studio>
2020-10-20 18:48:30 +0200

    restructure code - playRound function and game function(which loops playRound 5 times)

--- js/script.js
+++ js/script.js
@@ -1,69 +1,98 @@
-function game() { //loop round * 5 for whole game
-    for (let i = 0; i <= 4; i++) {
-
-        let round = i + 1;
-        
-        //get input from user and assign to variable - player's choice:
-        let playerSelection = prompt("Round " + round + ": Rock paper scissors, 1-2-3! Type your choice:");
-
-        //console test
+function playRound() {
+    function playerPlay() {             //get input from user and assign to variable - player's choice:
+        let playerSelection = prompt(/*"Round " + round + */": Rock paper scissors, 1-2-3! Type your choice:");
         console.log('You chose '+ playerSelection);
-
-        //generate computer's choice and assign it to variable
-        function computerPlay() {
-            //array of choices
-            let choiceArray = ["rock","paper","scissors"]
-            //random choice of index
-            let randomChoice = Math.floor(Math.random()*choiceArray.length);
-            // choose string associated with chosen index
-            let computerSelection = choiceArray[randomChoice];
-            //test computerSelection worked
-            //console.log("in function - computerSelection: " + computerSelection);
-            return(computerSelection);
+        return playerSelection;
         }
 
-        let computerSelection = computerPlay();
+    function computerPlay() {           //generate computer's choice and assign it to variable
+        let choiceArray = ["rock","paper","scissors"]                               //array of choices
+        let randomChoice = Math.floor(Math.random()*choiceArray.length);            //random choice of index
+        let computerSelection = choiceArray[randomChoice];                          // choose string associated with chosen index
+        console.log("The computer chose "+ computerSelection);
+        return computerSelection;
+        }
 
-        //compare computer's choice with player's choice
-        function playRound(computerSelection, playerSelection) {
+    const playerSelection = playerPlay();
+    const computerSelection = computerPlay();
+                            
+    function compare(playerSelection, computerSelection) {    //compare computer's choice with playe r's choice and print results  
+            
+        //set result variables    
+        let tie = "- tie";      
+        let win = " - you win round";
+        let loss = "- computer wins round";
+
+        if (playerSelection == computerSelection) {
+            return tie;
+        }
+        else {
             if (playerSelection == 'rock') {
-                if (computerSelection == playerSelection){
-                    console.log("Computer chose " + computerSelection + "- tie");
+                if (computerSelection == 'scissors') {
+                    return win;
+                    let playerScore = playerScore + 1;  // add point to playerScore   
                 }
-                else if (computerSelection == 'scissors'){
-                    console.log("Computer chose  " + computerSelection + " - you win round");
-                }
-                else if (computerSelection == 'paper'){
-                    console.log("Computer chose  " + computerSelection + " - computer wins round");
+                else if (computerSelection == 'paper') {
+                    return loss;
+                    let computerScore = computerScore +1; // add point to computerPoint
                 }
             }
             else if (playerSelection == 'paper') {
-                if (computerSelection == playerSelection){
-                    console.log("Computer chose " + computerSelection + "- tie");
-                }
-                else if (computerSelection == 'rock'){
-                    console.log("Computer chose " + computerSelection + "- you win round");
+                if (computerSelection == 'rock'){
+                    return win;
+                    let playerScore = playerScore + 1;  // add point to playerScore  
                 }
                 else if (computerSelection == 'scissors'){
-                    console.log("Computer chose " + computerSelection + "- computer wins round");
+                    return loss;
+                    let computerScore = computerScore +1; // add point to computerPoint
                 }
             }
             else if (playerSelection == 'scissors') {
-                if (computerSelection == playerSelection){
-                    console.log("Computer chose " + computerSelection + "- tie");
-                }
-                else if (computerSelection == 'paper'){
-                    console.log("Computer chose " + computerSelection + "- you win round");
+                if (computerSelection == 'paper'){
+                    return win;
+                    let playerScore = playerScore + 1;  // add point to playerScore 
                 }
                 else if (computerSelection == 'rock'){
-                    console.log("Computer chose " + computerSelection + "- computer wins round");
+                    return loss;
+                    let computerScore = computerScore +1; // add point to computerPoint
                 }
+                
             }
         }
+    }
 
-        playRound(computerSelection, playerSelection);
+    function getResult() {
+        let result = compare(playerSelection, computerSelection);
+        console.log("result is " + result);
+    }
+
+    getResult();
+}
+
+
+function game() {     //keep score   //report winner at the end
+    for (let i = 0; i <= 4; i++) { //loop round * 5 for whole game
+
+        let round = i + 1;
+
+        playRound();
+
+        /*if (round == 0) {
+            let playerScore = 0;   //initialize playerScore
+            let computerScore = 0;       //initialize computerScore
+        }
+        else if (result == win) {
+            playerScore+=1
+        }
+        else if (result == lose) {
+            computerScore+=1
+        }
+            
+        console.log(result + playerScore + computerScore)*/
+
     }
 }
+
 
 game();
 
@@ -89,3 +118,4 @@ computerSelection
 make repeated phrases into variables: tie message, win message, loss message, computer chose message
 make case sensitive
 throw error message if not rock paper or scissors*/
+
