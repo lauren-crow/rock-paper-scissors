131f45509b1d53098bbdc0c52eb566fadc3ec8a5
Lauren Crow <hi@lcrow.studio>
2020-11-09 13:33:07 +0100

    modify images, limit rounds to five and after five add reset button to restart game

---
+++ index.html
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rock Paper Scissors</title>
+<link rel="stylesheet" type="text/css" href="css/style.css"></link>
+</head>
+
+<body>
+
+<div class="buttons">
+    <button class="btns" id="rock"><img src="rock.png"/></button>
+    <button class="btns" id="paper"><img src="paper.png"/></button>
+    <button class="btns" id="scissors"><img src="scissors.png"/></button>
+</div>
+
+<div id="selections">
+    <div class="selectionResult" id="player"><h4>YOU CHOSE</h4><img id="playerImg" src = ""></div> 
+    <div class="selectionResult" id="computer"><h4>COMPUTER CHOSE</h4><img id="computerImg" src= ""></div> 
+</div>
+
+<div id="roundResult">
+</div>
+
+<div id="scoreBox">
+    <div id="youScore">
+        <h4>You</h4>
+        <div class="score" id="playerScoreBox"></div>
+    </div>
+    <div id="computerScore">
+        <h4>Computer</h4>
+        <div class="score" id="computerScoreBox"></div>
+    </div>
+</div>
+
+<img class="stars" id="playerWon" src = 'data:image/png;base64,R0lGODlhFAAUAIAAAP///wAAACH5BAEAAAAALAAAAAAUABQAAAIRhI+py+0Po5y02ouz3rz7rxUAOw=='></div>
+<img class="stars" id="computerWon" src = 'data:image/png;base64,R0lGODlhFAAUAIAAAP///wAAACH5BAEAAAAALAAAAAAUABQAAAIRhI+py+0Po5y02ouz3rz7rxUAOw=='></div>
+
+
+
+
+<script src='js/script.js'></script>
+</body>
--- js/script.js
+++ js/script.js
@@ -1,50 +1,58 @@
-//todo: loop & restart game, make responsive, thick border on winning selection result box
+//todo: get stars working, fix newgame score
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.btns');
 const playerBox = document.querySelector('#player');
 const computerBox = document.querySelector('#computer');
 const roundResultBox = document.querySelector('#roundResult');
+const selectionBox = document.querySelector('#selections');
 const playerImg = document.querySelector('#playerImg');
 const computerImg = document.querySelector('#computerImg');
+const scoreBox = document.querySelector('#scoreBox');
 const playerScoreBox = document.querySelector('#playerScoreBox');
 const computerScoreBox = document.querySelector('#computerScoreBox');
-const rockImg = "https://img.icons8.com/ios-filled/50/000000/rock.png";
-const paperImg = "https://img.icons8.com/ios/50/000000/paper-plane.png";
-const scissorsImg = "https://img.icons8.com/ios-filled/50/000000/cut.png";
+const playerWon = document.querySelector('#playerWon');
+const computerWon = document.querySelector('#computerWon');
+const rockImg = "rock.png";
+const paperImg = "paper.png";
+const scissorsImg = "scissors.png";
+const starsImg = "stars.png";
 
 //if computer is winner change computerbox
 
 let playerScore = 0;    //initializes scores
 let computerScore = 0;
 let tieCount = 0;
+selectionBox.style.visibility = "hidden";
+scoreBox.style.visibility = "hidden";
 
  //get input from user's button click and assign it to variable - return
 function playerPlay() {
     buttons.forEach((button) => {
         button.addEventListener('click', () =>  {
+            selectionBox.style.visibility = "visible";
+            scoreBox.style.visibility = "visible";
             playRound(button.id);
             const playerResult = document.createElement('div');
             playerResult.textContent = (button.id);
-            //playerBox.appendChild(playerResult);
             return button.id; 
         });
     });
 }
+
 
 //generate computer's random choice and return to later assign to variable
 function computerPlay() {       
     let choiceArray = ["rock","paper","scissors"]                               //array of choices
     let randomChoice = Math.floor(Math.random()*choiceArray.length);            //random choice of index
     let computerSelection = choiceArray[randomChoice];                          // choose string associated with chosen index
-    //console.log("The computer chose "+ computerSelection);
     const computerResult = document.createElement('div');
     computerResult.textContent = (computerSelection);
-    //computerBox.appendChild(computerResult);
     return computerSelection;
     }
 
-    const playerSelection = playerPlay(); 
+const playerSelection = playerPlay(); 
 
+//displays computer's selection and player's selection
 function showSelections(playerSelection, computerSelection){
 
     if(playerSelection == 'rock'){
@@ -68,60 +76,111 @@ function showSelections(playerSelection, computerSelection){
 function playRound(playerSelection) {             //get input from user and assign to variable - return
     const computerSelection = computerPlay();
     //compare computer's choice with playe r's choice and print results 
-    function compare(playerSelection, computerSelection) {     
-        //set result variables    
-        let tie = "    Tied round";      
-        let win = "   You won the round!";
-        let loss = "   Computer won the round.";
+    function compare(playerSelection, computerSelection) {    
 
         if (playerSelection == computerSelection) {
-            tieCount = tieCount +1; // add point to tie count
-            return tie;
+            result = 'tie';
         }
-        else {
-            if (playerSelection == 'rock') {
-                if (computerSelection == 'scissors') {
-                    playerScore = playerScore + 1;  // add point to playerScore  
-                    return win; 
-                }
-                else if (computerSelection == 'paper') {
-                    computerScore = computerScore + 1; // add point to computerPoint
-                    return loss;
-                }
+        else if (playerSelection === 'rock') {
+            if (computerSelection == 'scissors') { 
+                result = 'win'; 
             }
-            else if (playerSelection == 'paper') {
-                if (computerSelection == 'rock'){
-                    playerScore = playerScore + 1;  // add point to playerScore 
-                    return win;  
-                }
-                else if (computerSelection == 'scissors'){
-                    computerScore = computerScore + 1;  // add point to computerPoint
-                    return loss;
-                }
+            else if (computerSelection == 'paper') {
+                result = 'loss';
             }
-            else if (playerSelection == 'scissors') {
-                if (computerSelection == 'paper'){
-                    playerScore = playerScore + 1;   // add point to playerScore
-                    return win;  
+        } else if (playerSelection == 'paper') {
+            if (computerSelection == 'rock'){
+                result = 'win';  
                 }
-                else if (computerSelection == 'rock'){
-                    computerScore = computerScore + 1;  // add point to computerPoint
-                    return loss;
-                }
+            else if (computerSelection == 'scissors'){
+                result = 'loss';
+            }
+        }else if (playerSelection == 'scissors') {
+            if (computerSelection == 'paper'){
+                result = 'win';  
+            }
+            else if (computerSelection == 'rock'){
+                result = 'loss';
+            }
                 
-            }
         }
+        return result;
     }
+    result = compare(playerSelection, computerSelection);
+    //console.log(`playerSelection = ${playerSelection}`);
+    //console.log(`computerSelection = ${computerSelection}`);
+    //console.log(`result = ${result}`);
+
+    //shows round result message and tallies score  
+    switch(result){ 
+        case 'tie':
+            playerBox.style.border = ('none');
+            computerBox.style.border = ('none');
+            tieCount = tieCount +1;
+            roundMessage = "    Tied round"; 
+            //tally score
+            //display round result message
+            //border box to show winner
+        break;
+        case 'win':
+            playerBox.style.border = ('solid hotpink 1px');
+            computerBox.style.border = ('none');
+            playerScore = playerScore + 1;
+            roundMessage = "   You won the round!";
+        break;
+        case 'loss':
+            computerScore = computerScore + 1;  // add point to computerPoint
+            computerBox.style.border = ('solid hotpink 1px');
+            playerBox.style.border = ('none');
+            roundMessage = "   The computer won the round.";
+        break;
+        }
+
+        //ends game if either player gets 5 points
+        if(computerScore == 5 || playerScore == 5){
+            roundResultBox.style.fontSize = "1.5em";
+            roundResultBox.style.marginTop = "1%";
+            //add play again button
+            const resetButton = document.createElement('button');
+            resetButton.id = "resetBtn"
+            resetButton.textContent = "Play Again";
+            scoreBox.appendChild(resetButton);
+            document.querySelector('.buttons').style.visibility = "hidden";
+            
+            if(computerScore == 5){
+                roundMessage = "Sorry, the computer won this time.";
+                computerBox.style.backgroundColor= ('yellow');
+                computerWon.src = starsImg;
+            }else if(playerScore == 5){
+                roundMessage = "You won the game!!";
+                playerBox.style.backgroundColor = ('yellow');
+                playerWon.src = starsImg;
+            } 
+
+            resetButton.addEventListener('click', () =>  {
+                document.querySelector('.buttons').style.visibility = "visible";
+                selectionBox.style.visibility = "hidden";
+                scoreBox.style.visibility = "hidden";
+                resetButton.style.visibility = "hidden";
+                roundResultBox.style.visibility = "hidden";
+                playerWon.style.visibility = "hidden";
+                computerWon.style.visibility = "hidden";
+                playerBox.style.backgroundColor = ('hsl(195, 53%, 97%)');
+                computerBox.style.backgroundColor = ('hsl(195, 53%, 97%)');
+                computerScore = 0;
+                playerScore = 0;
+            }); 
+        }
+  
 
     showSelections(playerSelection, computerSelection);
 
     function getResult() {
         let result = compare(playerSelection, computerSelection);
-        roundResultBox.innerHTML = result;
+        roundResultBox.innerHTML = roundMessage;
     }
 
     function getScore() {        
-        //console.log("Your score: " + playerScore + " Computer score: " + computerScore + " Tie count: " + tieCount);
         playerScoreBox.innerHTML = playerScore;
         computerScoreBox.innerHTML = computerScore;
     }
@@ -131,11 +190,3 @@ function playRound(playerSelection) {             //get input from user and assi
 }
 
 
-//playRound();
-
-
-
-
-
-
-//when user clicks button, triggers playRound() and provides playRound with returned id
